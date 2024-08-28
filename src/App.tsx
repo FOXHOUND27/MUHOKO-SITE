2a9dc58e9df65ea6cbffc641d4de5f6eb23d7464
dalglish <dalglish45@gmail.com>
2024-08-28 12:22:56 +0200

    commiting updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import Hero from "./CustomComponents/Hero";
+import Navigation from "./CustomComponents/Navigation";
+import Partners from "./CustomComponents/Partners";
 function App() {
   return (
     <>
+      <Navigation />
       <Hero />
+      <Partners />
     </>
   );
 }
--- src/CustomComponents/Hero.tsx
+++ src/CustomComponents/Hero.tsx
@@ -1,9 +1,22 @@
-import Navigation from "./Navigation";
+import Spline from "@splinetool/react-spline";
 
 function Hero() {
   return (
-    <section>
-      <Navigation />
+    <section className="h-[100vh]">
+      <div className="fixed mt-20">
+        <div className="absolute left-10 top-48">
+          <h1 className="text-4xl font-semibold">
+            Transforming Visions into Reality with <br /> Innovative Technology
+          </h1>
+          <p className="text-lg text-pretty font-medium">
+            Partnering to Achieve Your Goals with <br /> Safety, Sustainability,
+            and Excellence
+          </p>
+        </div>
+        <div className="container w-full h-full">
+          <Spline scene="https://prod.spline.design/RaAPLDOrrirThDRD/scene.splinecode" />
+        </div>
+      </div>
     </section>
   );
 }
