3220b9be1ea9107e19fe22e807b28207ce296951
dalglish <dalglish45@gmail.com>
2024-08-29 17:29:23 +0200

    commiting solutions section styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import HeroSection from "./CustomComponents/HeroSection";
 import Navigation from "./CustomComponents/Navigation";
 import Partners from "./CustomComponents/PartnersSection";
+import SolutionSection from "./CustomComponents/SolutionSection";
 function App() {
   return (
     <>
       <Navigation />
       <HeroSection />
       <Partners />
+      <SolutionSection />
     </>
   );
 }
---
+++ src/CustomComponents/SolutionSection.tsx
@@ -0,0 +1,11 @@
+import "../CSS/Styles.css";
+
+function SolutionSection() {
+  return (
+    <section className="my-40 mx-auto">
+      <div className="bg-[#e5e5e5] md:w-[85%] md:h-[981px] mx-auto solutions-radius shadow-2xl"></div>
+    </section>
+  );
+}
+
+export default SolutionSection;
