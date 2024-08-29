fa92566f92912a396c7d868e3918c0886e124024
dalglish <dalglish45@gmail.com>
2024-08-29 14:01:31 +0200

    commiting code for mobile version of partners section

--- src/CustomComponents/Partners.tsx
+++ src/CustomComponents/Partners.tsx
@@ -10,12 +10,31 @@ import {
   gerApp,
 } from "../assets/index";
 
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 function Partners() {
+  // initializing aos
+  useEffect(() => {
+    AOS.init({
+      // Global settings:
+      duration: 1200, // values from 0 to 3000, with step 50ms
+      once: true, // whether animation should happen only once - while scrolling down
+    });
+
+    // Refresh AOS to handle new elements dynamically added to the DOM
+    AOS.refresh();
+  }, []);
+
   return (
     <section className="container">
-      <div className=" w-full md:w-[85%] h-[300px] md:h-[400px] hidden md:block bg-[#e5e5e5] partner-radius mx-auto my-20 shadow-xl">
+      <div className=" w-full md:w-[85%] h-[300px] md:h-[400px] hidden md:block bg-[#e5e5e5] partner-radius mx-auto my-20 shadow-2xl">
         {/* Div containing titlt and subtitle text */}
-        <div className="md:text-center text-center py-6 md:px-0">
+        <div
+          data-aos="zoom-in"
+          className="md:text-center text-center py-6 md:px-0"
+        >
           <h1 className="md:text-2xl text-xs font-semibold">
             Our Partners and Collaborations
           </h1>
@@ -68,15 +87,52 @@ function Partners() {
       </div>
 
       {/* Mobile partners section */}
-      <div className=" w-full md:w-[85%] h-[300px] md:h-[400px] block md:hidden bg-[#e5e5e5]  mx-auto my-20 shadow-xl relative top-20">
+      <div className=" w-full md:w-[85%] py-4 h-auto md:h-[400px] block md:hidden bg-[#e5e5e5]  mx-auto my-20 shadow-xl relative top-20 z-[-2]">
         {/* Div containing titlt and subtitle text */}
         <div className="md:text-center text-center py-6 md:px-0">
-          <h1 className="md:text-2xl text-xs font-semibold">
+          <h1 className=" text-[18px] font-semibold">
             Our Partners and Collaborations
           </h1>
           <p className="md:text-lg text-xs md:w-auto w-[80%] text-[#716E6F] font-medium md:mx-0 mx-auto">
             Companies We Currently Work With and Have Partnered With in the Past{" "}
           </p>
+        </div>
+        {/* Div for brands */}
+        <div className="flex justify-center">
+          {/* Column 1 */}
+          <div className="flex space-y-4 flex-col">
+            <div className="h-20 w-44 bg-white rounded-xl shadow-xl flex justify-center items-center">
+              <img src={fudlink} className="w-20" alt="fudlink" />
+            </div>
+
+            <div className="h-20 w-44 bg-white rounded-xl shadow-xl flex justify-center items-center pt-2">
+              <img src={mekenficent} alt="mekenficent" />
+            </div>
+
+            <div className="h-20 w-44 bg-white rounded-xl shadow-xl flex justify-center items-center">
+              <img src={NUST} alt="NUST" />
+            </div>
+
+            <div className="h-20 w-44 bg-white rounded-xl shadow-xl flex justify-center items-center">
+              <img src={fashion} className="w-14" alt="Fashion" />
+            </div>
+
+            <div className="h-20 w-44 bg-white rounded-xl shadow-xl flex justify-center items-center">
+              <img src={OTESA} className="w-28 h-20" alt="Otesa" />
+            </div>
+
+            <div className="h-20 w-44 bg-white rounded-xl shadow-xl flex justify-center items-center">
+              <img src={NTTS} className="w-16" alt="NTTS" />
+            </div>
+
+            <div className="h-20 w-44 bg-white rounded-xl shadow-xl flex justify-center items-center">
+              <img src={intraHealth} className="w-32" alt="NUST" />
+            </div>
+
+            <div className="h-20 w-44 bg-white rounded-xl shadow-xl flex justify-center items-center">
+              <img src={gerApp} className="w-44" alt="gerApp" />
+            </div>
+          </div>
         </div>
       </div>
     </section>
