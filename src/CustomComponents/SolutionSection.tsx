11e9ea5204215db0e83d5f5896e0649eef93de8b
dalglish <dalglish45@gmail.com>
2024-09-03 14:48:42 +0200

    commiting code

--- src/CustomComponents/SolutionSection.tsx
+++ src/CustomComponents/SolutionSection.tsx
@@ -7,23 +7,42 @@ import {
   mainCharacter,
 } from "../assets/index";
 
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 function SolutionSection() {
+  // initializing aos
+  useEffect(() => {
+    AOS.init({
+      // Global settings:
+      duration: 1200, // values from 0 to 3000, with step 50ms
+      once: true, // whether animation should happen only once - while scrolling down
+    });
+
+    // Refresh AOS to handle new elements dynamically added to the DOM
+    AOS.refresh();
+  }, []);
+
   return (
     <section className="my-52 flex justify-center items-center container flex-col">
-      <div className="bg-[#e5e5e5] md:w-[83%] md:h-[981px] solutions-radius shadow-2xl relative flex justify-end">
+      <div className="bg-[#e5e5e5] md:w-[83%] md:h-[920px] hidden md:block  solutions-radius shadow-2xl relative">
         {/* Div for Solutionts text and Image */}
         <div className="">
           {/* Character Image */}
           <img
             src={mainCharacter}
-            className="absolute top-[-150px] left-28"
+            className="absolute top-[-150px] left-20"
             alt="Main chatacter"
           />
 
           {/* Text for the solutions section */}
-          <div className="flex flex-col items-center mt-28 absolute right-16">
+          <div
+            data-aos="zoom-in"
+            className="flex flex-col items-center mt-20 absolute right-16"
+          >
             <h1 className="text-5xl font-semibold">Solutions</h1>
-            <p className="w-[450px] text-center font-medium">
+            <p className="w-[430px] text-center font-medium">
               We deliver cutting-edge custom software development, immersive
               game development, dynamic web development, and a full spectrum of
               IT solutions and services tailored to meet the unique needs of our
@@ -32,11 +51,11 @@ function SolutionSection() {
           </div>
         </div>
 
-        <div className=" flex flex-col space-y-10 mt-80 mx-auto pl-20">
+        <div className=" flex flex-col space-y-10 mt-80 relative left-32">
           {/* Row 1 Cards */}
           <div className="flex space-x-10">
             {/* Card 1 */}
-            <div className="w-[400px] h-[200px] bg-black rounded-lg shadow-2xl flex flex-col justify-center items-center p-5">
+            <div className="w-[400px] h-[200px] bg-muhoko-gradient rounded-lg shadow-2xl flex flex-col justify-center items-center p-5">
               <img src={websites} className="w-[52px]" alt="websites icon" />
 
               <h1 className="text-white text-lg my-1">Web Development</h1>
@@ -47,25 +66,27 @@ function SolutionSection() {
               </p>
             </div>
             {/* Card 2 */}
-            <div className="w-[400px] h-[200px] bg-black rounded-lg shadow-2xl flex flex-col justify-center items-center p-5">
+            <div className="w-[400px] h-[200px] bg-muhoko-gradient  rounded-lg shadow-2xl flex flex-col justify-center items-center p-5">
               <img
                 src={development}
                 className="w-[52px]"
                 alt="development icon"
               />
 
-              <h1 className="text-white text-lg my-1">Web Development</h1>
+              <h1 className="text-white text-lg my-1">
+                Software & App Development
+              </h1>
 
               <p className="text-white text-center">
-                We specialize in creating custom websites that are visually
-                appealing, highly functional, and optimized for performance.
+                We specialize in creating custom software applications and
+                mobile apps that are intuitive, scalable, and secure
               </p>
             </div>
           </div>
 
           {/* Row 2 Cards */}
           <div className="flex space-x-10">
-            <div className="w-[400px] h-[200px] bg-black rounded-lg shadow-2xl flex flex-col justify-center items-center p-5">
+            <div className="w-[400px] h-[200px] bg-muhoko-gradient  rounded-lg shadow-2xl flex flex-col justify-center items-center p-5">
               {" "}
               <img src={gamDev} className="w-[52px]" alt="game dev icon" />
               <h1 className="text-white text-lg my-1">Game Development</h1>
@@ -75,7 +96,7 @@ function SolutionSection() {
                 creation to game mechanics and more.
               </p>
             </div>
-            <div className="w-[400px] h-[200px] bg-black rounded-lg shadow-2xl flex flex-col justify-center items-center p-5">
+            <div className="w-[400px] h-[200px] bg-muhoko-gradient  rounded-lg shadow-2xl flex flex-col justify-center items-center p-5">
               {" "}
               <img
                 src={character}
