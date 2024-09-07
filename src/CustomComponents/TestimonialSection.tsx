0981536cf432865c5d11127e95515365236c6c49
dalglish <dalglish45@gmail.com>
2024-09-07 16:32:58 +0200

    commiting changes to code

--- src/CustomComponents/TestimonialSection.tsx
+++ src/CustomComponents/TestimonialSection.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import Autoplay from "embla-carousel-autoplay";
-
 import { Card, CardContent } from "@/components/ui/card";
 import {
   Carousel,
@@ -9,11 +8,26 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 export function TestimonialSection() {
   const plugin = React.useRef(
     Autoplay({ delay: 2000, stopOnInteraction: true })
   );
+
+  // initializing aos
+  useEffect(() => {
+    AOS.init({
+      // Global settings:
+      duration: 1200, // values from 0 to 3000, with step 50ms
+      once: true, // whether animation should happen only once - while scrolling down
+    });
+
+    // Refresh AOS to handle new elements dynamically added to the DOM
+    AOS.refresh();
+  }, []);
 
   return (
     <section>
@@ -21,9 +35,12 @@ export function TestimonialSection() {
         {/* Carousel Container and Styles */}
         <div className="w-[85%] h-auto p-10 bg-muhoko-gradient mx-auto flex flex-col justify-center items-center mt-32 mb-10 rounded-[49px] shadow-2xl">
           {/* Text Div */}
-          <div className="flex flex-col relative mb-10 right-72 pt-5">
-            <h1 className="text-white text-5xl">Testimonials</h1>
-            <p className="text-white text-left text-lg ml-2">
+          <div
+            data-aos="zoom-out"
+            className="flex flex-col relative mb-10 right-72 pt-5"
+          >
+            <h1 className="text-white text-5xl font-semibold">Testimonials</h1>
+            <p className="text-white text-left text-lg ml-2 font-medium">
               Don't just take our word for it - see what actual clients of our{" "}
               <br />
               services have to say about their experience.
@@ -44,7 +61,7 @@ export function TestimonialSection() {
                 <CarouselItem key={index}>
                   <div className="p-1">
                     {/* Carousel Card */}
-                    <Card className=" bg-[#383938] border-none">
+                    <Card className=" bg-[#383938] border-none shad">
                       {/* Carousel Content */}
                       <CardContent className="flex aspect-square items-center justify-center p-6">
                         <span className="text-4xl font-semibold text-white">
