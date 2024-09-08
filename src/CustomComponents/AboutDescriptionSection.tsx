d7498f5b766a8bf059e5773bd0a7b66ff60d144b
dalglish <dalglish45@gmail.com>
2024-09-08 19:19:47 +0200

    commiting about us page proggress

--- src/CustomComponents/AboutDescriptionSection.tsx
+++ src/CustomComponents/AboutDescriptionSection.tsx
@@ -1,5 +1,38 @@
 function AboutDescriptionSection() {
-  return <div>AboutDescriptionSection</div>;
+  return (
+    <section>
+      {/* Main Title Text Div */}
+      <div className="text-center my-5">
+        <h1 className="text-5xl my-2 font-semibold">About Us </h1>
+        <p className="text-3xl font-medium">
+          GET TO KNOW US AND <br /> WHAT WE DO !
+        </p>
+      </div>
+      {/* Main Paragraph */}
+      <div className="text-center flex w-full justify-center items-center">
+        <p className="w-[90%] text-lg leading-normal text-center">
+          Muhoko Innovation Studio is a multidisciplinary internet company that
+          caters to becoming one of the best and leading technology companies in
+          Africa, providing Artificial Intelligence (AI) and futuristic
+          technology. Muhoko is a leading technology company in Namibia that
+          uses advanced scientific strategies, innovation, and simplified
+          efficiency to build environmental needs and solutions to everyday
+          challenges. Using industry accepted tools ,methodologies, and best
+          practices across various industries and disciplines We are a company
+          that values the passion and commitment of its clients and employees.
+          the company encourages fresh ideas, constructive criticism , and input
+          from all members to achieve better outcomes. The companies goal is to
+          unlock the potential of every industry in Namibia’s market by
+          investing and developing a central, interconnected community of
+          innovators, idealists, and entrepreneurs. This is accomplished through
+          its four main departments :Education ,Studio, Society, and Technology.
+          This departments work together to offer innovative solutions and
+          services to our clients respectfully , and to create a platform for
+          for the growth and development of local entrepreneurs and innovator's
+        </p>
+      </div>
+    </section>
+  );
 }
 
 export default AboutDescriptionSection;
