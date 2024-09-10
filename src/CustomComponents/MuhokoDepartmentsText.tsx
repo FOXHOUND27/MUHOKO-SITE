5499993f71961e177b5aa256505291fe74c06d9d
dalglish <dalglish45@gmail.com>
2024-09-10 20:58:19 +0200

    commiting completed mobile version of departments text

--- src/CustomComponents/MuhokoDepartmentsText.tsx
+++ src/CustomComponents/MuhokoDepartmentsText.tsx
@@ -1,22 +1,25 @@
 function MuhokoDepartmentsText() {
-    return (
-      <>
-        <section>
-          <div className="flex flex-col items-center my-5 font-semibold">
-            <h2 className="text-5xl text-black">Muhoko Departments</h2>
-            <p className="">GET TO KNOW OUR COMPANY DEPARTMENTS!</p>
-            <p className="text-black">
-              Each department within Muhoko functions as a separate entity, with
-              its own specific goals, responsibilities, and areas of expertise.
-              <br />
-              Each department also works independently to achieve its goals but
-              also collaborates to support the overall vision and mission of the
-              company.
-            </p>
-          </div>
-        </section>
-      </>
-    );
-  }
-  
-  export default MuhokoDepartmentsText;
+  return (
+    <>
+      <section>
+        <div className="flex flex-col items-center my-5 font-semibold">
+          <h2 className="text-2xl md:text-5xl text-black">
+            Muhoko Departments
+          </h2>
+          <p className="text-sm md:text-base">
+            GET TO KNOW OUR COMPANY DEPARTMENTS !
+          </p>
+          <p className="text-black w-[80%] text-center text-sm md:text-base">
+            Each department within Muhoko functions as a separate entity, with
+            its own specific goals, responsibilities, and areas of expertise.
+            Each department also works independently to achieve its goals but
+            also collaborates to support the overall vision and mission of the
+            company.
+          </p>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default MuhokoDepartmentsText;
